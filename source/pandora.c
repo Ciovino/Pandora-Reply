a19859c5cc83ca5c98fbb46a34fc1739c8a03700
Ciovino <tarantino.giovanbattista01@gmail.com>
2023-09-30 16:54:39 +0200

    lettura file input (con test)

--- source/pandora.c
+++ source/pandora.c
@@ -1,4 +1,25 @@
-#include <stdio.h> // fprintf(), fopen(), fclose()
+#include <stdio.h>  // fprintf, fscanf, fopen, fclose
+#include <stdlib.h> // malloc, free
+
+#define MAX_TURNI               1000000
+#define MAX_DEMONI              100000
+#define MAX_STAMINA             100000
+#define MAX_FRAMMENTI_TURNI     100000
+#define MAX_FRAMMENTI_PER_TURNO 10000
+
+typedef struct {
+    int stamina;
+    int maxStamina;
+    int frammentiTotali;
+} PLAYER;
+
+typedef struct {
+    int staminaPersa;
+    int turniPerRecuperoStamina;
+    int staminaRecuperata;
+    int turniFrammenti;
+    int *frammentiPerTurno;
+} DEMON;
 
 int main(int argc, char **argv)
 {
@@ -16,7 +37,54 @@ int main(int argc, char **argv)
         return -2;
     }
 
+    // File trovato ed aperto
+    PLAYER pandora = {0, 0, 0};
+    DEMON *demoni;
+    int turniTotali, totaleDemoni;
+
+    // Pandora
+    fscanf(
+        inputFile, "%d %d %d %d%*c", &pandora.stamina, &pandora.maxStamina,
+        &turniTotali, &totaleDemoni);
+
+    // Demoni
+    demoni = malloc(totaleDemoni * sizeof(*demoni));
+    for (int i = 0; i < totaleDemoni; i++) {
+        fscanf(
+            inputFile, "%d %d %d %d%*c", &demoni[i].staminaPersa,
+            &demoni[i].turniPerRecuperoStamina, &demoni[i].staminaRecuperata,
+            &demoni[i].turniFrammenti);
+
+        demoni[i].frammentiPerTurno =
+            malloc(demoni[i].turniFrammenti * sizeof(int));
+
+        for (int j = 0; j < demoni[i].turniFrammenti; j++)
+            fscanf(inputFile, "%d%*c", &demoni[i].frammentiPerTurno[j]);
+    }
+
     fclose(inputFile);
+
+    // Test Output
+    fprintf(stdout, "Numero turni: %d\n", turniTotali);
+    fprintf(
+        stdout,
+        "Pandora:\n\tStamina: %d\n\tStamina massima: %d\n\tFrammenti totali: "
+        "%d\n",
+        pandora.stamina, pandora.maxStamina, pandora.frammentiTotali);
+    fprintf(stdout, "Numero totale demoni: %d\n", totaleDemoni);
+
+    for (int i = 0; i < totaleDemoni; i++) {
+        fprintf(
+            stdout,
+            "Demone %d:\n\tStamina persa:%d\n\tTurni per recuperare la "
+            "stamina:%d\n\tStamina recuperata:%d\n\tTurni per fraggmenti:%d\n",
+            i + 1, demoni[i].staminaPersa, demoni[i].turniPerRecuperoStamina,
+            demoni[i].staminaRecuperata, demoni[i].turniFrammenti);
+
+        free(demoni[i].frammentiPerTurno);
+    }
+
+    free(demoni);
 
     return 0;
 }
